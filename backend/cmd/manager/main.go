a0d153859c17cfc6b8c9d0ed278b949b1314fb1f
maroon <maroon.ayoub@ibm.com>
2021-12-15 18:49:56 +0200

    refactored

--- backend/cmd/manager/main.go
+++ backend/cmd/manager/main.go
@@ -1,24 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"github.com/ICST-Technion/EZRecruit.git/pkg/db/in-memory"
 	"github.com/ICST-Technion/EZRecruit.git/pkg/rest-api"
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// in memory DB
 	inMemoryDB := inmemory.NewInMemoryDB()
 	// restAPI server
-	restAPIServer := restapi.NewRestAPIServer(inMemoryDB)
-
-	// the restAPI public methods
-	router := gin.Default()
-	router.GET("/jobs", restAPIServer.GetJobListings)
-
-	if err := router.Run("localhost:8080"); err != nil {
-		fmt.Printf("failed to start server - %v", err)
-		return
-	}
+	restAPIServer := restapi.NewRESTAPIServer(inMemoryDB)
+	// start server
+	restAPIServer.Start()
 }
--- backend/pkg/rest-api/server.go
+++ backend/pkg/rest-api/server.go
@@ -1,21 +1,56 @@
 package restapi
 
 import (
+	"fmt"
 	"github.com/ICST-Technion/EZRecruit.git/pkg/db"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"os"
 )
 
-// NewRestAPIServer returns a new instance of RestAPIServer.
-func NewRestAPIServer(dbClient db.DB) *Server {
+const (
+	envVarContainerPort = "PORT"
+	domain              = "localhost"
+	certsFolder         = "certs"
+)
+
+// NewRESTAPIServer returns a new instance of RestAPIServer.
+func NewRESTAPIServer(dbClient db.DB) *Server {
 	return &Server{
 		dbClient: dbClient,
 	}
 }
 
-// Server implements the restAPI functionality logic
+// Server implements the restAPI functionality logic.
 type Server struct {
 	dbClient db.DB
+}
+
+// Start function to start the listener.
+func (s *Server) Start() {
+	// the restAPI public methods
+	router := gin.Default()
+
+	// Ping handler
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+	// GET METHODS
+	router.GET("/jobs", s.GetJobListings)
+
+	// Get port
+	port := os.Getenv(envVarContainerPort)
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Printf("started server - url:%s port:%s", os.Getenv("PUBLIC_URL"), port)
+
+	if err := router.Run(fmt.Sprintf(":%s", port)); err != nil {
+		log.Panicf("failed to start server - %v", err)
+		return
+	}
 }
 
 // GetJobListings responds with the list of all job-listings as JSON.
