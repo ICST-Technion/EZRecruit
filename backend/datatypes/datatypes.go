e9f9e8ae99bc6565916e15425fe80f233b74d31e
maroon <maroon.ayoub@ibm.com>
2021-12-15 23:28:59 +0200

    added labels to job listing

--- backend/datatypes/datatypes.go
+++ backend/datatypes/datatypes.go
@@ -2,9 +2,10 @@ package datatypes
 
 // JobListing represents a job-listing entry.
 type JobListing struct {
-	ID             string   `json:"id"`
+	ID             string   `json:"_id"` //wix items need to have an _id field
 	Title          string   `json:"title"`
 	Description    string   `json:"description"`
 	Location       string   `json:"location"`
 	RequiredSkills []string `json:"requiredSkills"`
+	Labels         []string `json:"labels"`
 }
