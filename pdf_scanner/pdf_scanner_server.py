513f3a2b8b921f631606ca05e91346720944dda3
aradk <somsom1151@gmail.com>
2021-12-18 22:07:33 +0200

    Pdf scanner in a non-server (yet) form

---
+++ pdf_scanner/pdf_scanner_server.py
@@ -0,0 +1,50 @@
+from multilingual_pdf2text.pdf2text import PDF2Text
+from multilingual_pdf2text.models.document_model.document import Document
+import logging
+import re
+logging.basicConfig(level=logging.INFO)
+
+path = "C:/Users/aradk/Desktop/Yearly_Project/aa.pdf"
+search_for = "איש"
+lng = "heb"
+
+def findWholeWord(w):
+    return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
+
+def get_path_from_user(path):
+    print("Do you want to insert a path to read from? y/n")
+    ans = input()
+    if ans == "y":
+        print("Enter path now: ")
+        path = input()
+    elif ans == "n":
+        print("Using path: ", path)
+    else:
+        print("Bad input! Fuck you Mirotic!!!")
+
+def get_language_and_word(lng, search_for):
+    print("Enter language (3 letters)")
+    lng = input()
+    print("Enter sentence to search for")
+    search_for = input()
+
+def main():
+    #get_path_from_user(path)
+    get_language_and_word(lng, search_for)
+
+    ## create document for extraction with configurations
+    pdf_document = Document(
+        document_path=path,
+        language=lng
+        )
+    pdf2text = PDF2Text(document=pdf_document)
+    content = pdf2text.extract()
+    str = content[0]['text']
+    if (findWholeWord(search_for)(str)) != None:
+        print("Found word")
+    else:
+        print("Did not find word")
+    #print(content[0]['text'])
+
+if __name__ == "__main__":
+    main()
