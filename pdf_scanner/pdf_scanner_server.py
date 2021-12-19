9b669f037b3015516eea560e271cb1965343dffa
aradk <somsom1151@gmail.com>
2021-12-19 21:44:38 +0200

    Pdf scanner program

--- pdf_scanner/pdf_scanner_server.py
+++ pdf_scanner/pdf_scanner_server.py
@@ -3,6 +3,7 @@ from multilingual_pdf2text.models.document_model.document import Document
 import logging
 import re
 logging.basicConfig(level=logging.INFO)
+import sys
 
 path = "C:/Users/aradk/Desktop/Yearly_Project/aa.pdf"
 search_for = "איש"
@@ -28,9 +29,22 @@ def get_language_and_word(lng, search_for):
     print("Enter sentence to search for")
     search_for = input()
 
+
+"""
+    Gets 2 arguments- a pdf path and a language (usually 'heb' or 'eng') and returns
+    the words from this language in this pdf as a string.
+"""
 def main():
     #get_path_from_user(path)
-    get_language_and_word(lng, search_for)
+    #get_language_and_word(lng, search_for)
+    if len(sys.argv) != 3:
+        print("Wrotng number of arguments")
+        exit(-1)
+
+    path = sys.argv[1]
+    lng = sys.argv[2]
+
+    #print("args are: " + path + "  " + lng)
 
     ## create document for extraction with configurations
     pdf_document = Document(
@@ -40,11 +54,13 @@ def main():
     pdf2text = PDF2Text(document=pdf_document)
     content = pdf2text.extract()
     str = content[0]['text']
-    if (findWholeWord(search_for)(str)) != None:
+    #print(str)
+    return str
+    """if (findWholeWord(search_for)(str)) != None:
         print("Found word")
     else:
         print("Did not find word")
-    #print(content[0]['text'])
+    #print(content[0]['text'])"""
 
 if __name__ == "__main__":
     main()
