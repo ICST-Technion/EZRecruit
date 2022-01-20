ffff5ecfaa71c8567d7d93de65011533eac2cfce
maroon <maroon.ayoub@ibm.com>
2022-01-20 13:41:35 +0200

    minor reordering

--- pkg/db/in-memory/in_memory_db.go
+++ pkg/db/in-memory/in_memory_db.go
@@ -158,10 +158,10 @@ func (db *DB) InsertApplication(jobApplication *datatypes.JobApplication, resume
 		// assign unique ID
 		jobApplication.ID = strconv.Itoa(db.availableApplicationID)
 		db.availableApplicationID++
-		// append status ID to labels
-		jobApplication.Labels = append([]string{getStatusLabel(jobApplication.Status)}, jobApplication.Labels...)
 		// append job ID to labels
 		jobApplication.Labels = append([]string{fmt.Sprintf("job:%s", jobApplication.JobId)}, jobApplication.Labels...)
+		// append status ID to labels
+		jobApplication.Labels = append([]string{getStatusLabel(jobApplication.Status)}, jobApplication.Labels...)
 	}
 	// (rewrites if ID exists)
 	db.jobApplicationsMap[jobApplication.User] = *jobApplication
