cfa0609e46b18847d2d3b0f1fd175dd8c6cf4892
YP2410 <yaron.porat@campus.technion.ac.il>
2022-01-20 14:32:40 +0200

    deleted unnecessary keys that GitGuardian warned

--- namba-final-project/config.py
+++ namba-final-project/config.py
@@ -1,6 +1,6 @@
 
 class ApplicationConfig:
-    SECRET_KEY = 'TheFlyingDutchman\n\xec]/'
+
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:porat2410@localhost/Namba'  # postgres connection string
 
     SESSION_TYPE = "sqlalchemy"
@@ -8,6 +8,5 @@ class ApplicationConfig:
 
 HOST_PORT = 4000  # port for flask
 
-BOT_KEY = "5029169709:AAHvskSVaIUTmMDeJW-6XBoOzi-IC4naEjA"  # key for telegram bot
 
 
--- namba-final-project/src/components/pages/adminSignInPage/adminSignInPage.js
+++ namba-final-project/src/components/pages/adminSignInPage/adminSignInPage.js
@@ -4,7 +4,7 @@ import {AdminSignInForm} from "./adminSignInForm";
 const AdminSignInPage = () => {
     return(
         <header className="App-header">
-            <h1>Admin pagee</h1>
+            <h1>Admin page</h1>
             <AdminSignInForm/>
         </header>
     )
