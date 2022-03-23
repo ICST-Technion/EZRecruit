443ffae8b7d424c8c9854eb435e5c1df96532e7b
Maroon Ayoub <maroon.ayoub@ibm.com>
2022-03-23 16:36:32 +0200

    delete extra folder
    Signed-off-by: Maroon Ayoub <maroon.ayoub@ibm.com>

--- frontend/namba-final-project/README.md
+++
@@ -1,5 +0,0 @@
-Just run "run_project.py" and it wiil run :-)
-
-You can change the parameters as you like in "config.py"
-
-The react port is 3000
--- frontend/namba-final-project/src/config.js
+++
@@ -1,4 +0,0 @@
-
-export const APIBase = "http://localhost:4000";
-
-export const pages = ['AdminSignIn', 'MainAdminPage', 'Page']; // redundant...
